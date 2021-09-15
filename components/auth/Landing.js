4bec1d408150a6b2111238ee64235e128ae6758f
alfred <alfredtapia1999@outlook.com>
2021-09-15 12:39:39 -0500

    1st commit, In this part of the project I create the landing page which is inside components/auth/Landing.js. The packages used were : npm install @react-navigation/stack for createStackNavigator and imported as import { createStackNavigator } from '@react-navigation/stack'

---
+++ components/auth/Landing.js
@@ -0,0 +1,26 @@
+// This is the first component function of this application.
+// And makes the landing page for select between 
+// Register option or Login option.
+
+// Write in the keyboard rfc and it generates all 
+// the nessesary code:
+
+import React from 'react';
+import { Button, View } from 'react-native';
+
+/* View acts like div in HTML. */
+
+export default function Landing( navigation ) {// navigation is a props.
+    return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+            <Button 
+                title="Register"
+                onPress={() => navigation.navigate("Register")}
+            />
+            <Button 
+                title="Login"
+                onPress={() => navigation.navigate("Login")}
+            />
+        </View>
+    );
+}
