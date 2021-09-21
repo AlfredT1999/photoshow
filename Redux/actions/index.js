ab30d16aab912c0eeb39d41ffb46884dd505d116
alfred <alfredtapia1999@outlook.com>
2021-09-21 11:48:31 -0500

    Displaying user profile with firebase Firestore, fetching users posts. Fetch is the retrieval of data by a software program, script, or hardware device. After being retrieved, the data is moved to an alternate location or displayed on a screen.

--- Redux/actions/index.js
+++ Redux/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import {USER_STATE_CHANGE} from '../constants/index'
+import {USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE} from '../constants/index'
 
 export function fetchUser(){
     return ((dispatch) => {
@@ -15,6 +15,28 @@ export function fetchUser(){
             else{
                 console.log("does not exist");
             }
+        })
+    })
+}
+
+export function fetchUserPosts(){
+    return ((dispatch) => {
+        firebase.firestore()
+        .collection("posts")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("userPosts")
+        .orderBy("creation", "asc")// order the date to the latest to first.
+        .get()
+        .then((snapshot) => {// a snapshot is the state of a system at a particular point in time.
+            let posts = snapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+
+                return {id, ...data}
+            })
+
+            console.log(posts);
+            dispatch({ type: USER_POSTS_STATE_CHANGE, posts })
         })
     })
 }
