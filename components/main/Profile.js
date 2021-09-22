e223d2a67b48fba8ce7156e822680b8586ceed76
alfred <alfredtapia1999@outlook.com>
2021-09-22 15:04:54 -0500

    Generalizing users profile screen.

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,24 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
 import {connect} from 'react-redux'
 import firebase from 'firebase'
 require('firebase/firestore')
 
 function Profile(props) {
-    const {currentUser, posts} = props;
-    console.log({currentUser, posts});
+    const [userPost, setUserPosts] = useState([])
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const {currentUser, posts} = props;
+        console.log({currentUser, posts});
+
+        if(props.route.params.uid === firebase.auth().currentUser.uid){
+            setUser(currentUser)
+            setUserPosts(posts)
+        }
+        else{
+            firebase.firestore()
+            .collection("users")
+            .doc(props.route.params.uid)
+            .get()
+            .then((snapshot) => {// a snapshot is the state of a system at a particular point in time.
+                if(snapshot.exists){// The exists() method is part of the snapshot object which is returned by firebase queries.
+                    setUser(snapshot.data());
+                }
+                else{
+                    console.log("does not exist");
+                }
+            })
+            firebase.firestore()
+            .collection("posts")
+            .doc(props.route.params.uid )
+            .collection("userPosts")
+            .orderBy("creation", "asc")// order the date to the latest to first.
+            .get()
+            .then((snapshot) => {// a snapshot is the state of a system at a particular point in time.
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+
+                    return {id, ...data}
+                })
+
+                setUserPosts(posts);
+            })
+        }
+    }, [props.route.params.uid]);
+
+    if(user === null){
+        return <View>User null</View>
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.containerInfo}>
-                <Text>{currentUser.name}</Text>
-                <Text>{currentUser.email}</Text>
+                <Text>{user.name}</Text>
+                <Text>{user.email}</Text>
             </View>
             <View style={styles.containerGallery}>
                 <FlatList
                     numColumns={3}
                     horizontal={false}
-                    data={posts}
+                    data={userPost}
                     renderItem={({item}) => (
                         <View style={styles.containerImage}>
                             <Image
